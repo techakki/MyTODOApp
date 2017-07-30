bd1e06d4148517bdf9485539cd55aa0c21b54fe7
aakakki <akshaykmr869@gmail.com>
2017-07-30 18:43:34 +0530

    added react-bootstrap library for styling and pseudo code

--- reactjs-todo-app/src/App.js
+++ reactjs-todo-app/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LoginForm } from './LoginForm.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1> Hello Guys..! WELCOME TO AKSHAY'S TODO APP  </h1>
+        <br />
+        <h4> Login Form </h4>
+        <br />
+        <LoginForm />
       </div>
     );
   }
